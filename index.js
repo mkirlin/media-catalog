baf7e24e80ff164e1914f10634c81659c9f141d4
Michael Kirlin <mkirlin@gmail.com>
2015-05-14 01:27:58 -0500

    Rewrote entire app in Angular, rebuilt login/logout/auth, recreated add user and add item

--- index.js
+++ index.js
@@ -1,23 +1,74 @@
-var firebaseDB = new Firebase("https://resplendent-inferno-8604.firebaseio.com/")
+var MediaCatalog = angular.module("MediaCatalog", ["firebase"]);
 
-$(document).ready(function() {
-    $('.main-site').hide();
-    $('.sign-in').submit(function(e) {
-        e.preventDefault();
-        logInUser($('#inputEmail').val(), $('#inputPassword').val())
-    })
-});
+MediaCatalog.controller('CatalogCtrl', function CatalogCtrl($scope, $firebaseAuth, $firebaseArray) {
+    var url = "https://resplendent-inferno-8604.firebaseio.com/";
+    var db = new Firebase(url);
+    $scope.auth = $firebaseAuth(db);
+    $scope.items = $firebaseArray(new Firebase(url + 'items'))
 
-var logInUser = function(email, password) {
-    firebaseDB.authWithPassword({
-        email    : email,
-        password : password
-    }, function(error, authData) {
-        if (error) {
-            $('.main-site').append("<p>Fuck off</p>")
-        } else {
-            $('.log-in').hide();
-            $('.main-site').show();
-        }
+    $scope.auth.$onAuth(function(authData) {
+      $scope.authData = authData;
     });
-}
+
+    $scope.addItem = function() {
+        $scope.items.$add({
+            name: $scope.itemName,
+            type: $scope.itemType,
+            description: $scope.itemDescription
+        }).then(function() {
+            $('#addItemModal').modal('hide')
+            $scope.alert = 'Item added.'
+        }).catch(function(error) {
+            $scope.error = error.message;
+        });
+    };
+
+    $scope.deleteItem = function() {
+        var item = $scope.items[this.$index]
+        $scope.items.$remove(item).then(function() {
+            $scope.alert = 'Item deleted.'
+        }).catch(function(error) {
+            $scope.error = error.message;
+        });
+    };
+
+    $scope.login = function() {
+        $scope.message = null;
+        $scope.error = null;
+
+        $scope.auth.$authWithPassword({
+            email: $scope.loginEmail,
+            password: $scope.loginPassword
+        }).then(function(authData) {
+            $scope.loginEmail = ''
+            $scope.loginPassword = ''
+        }).catch(function(error) {
+            if(error.code === 'INVALID_PASSWORD' || error.code === 'INVALID_USER' || error.code === 'INVALID_EMAIL') {
+                $scope.error = error.message
+            } else {
+                console.log(error.code)
+                $scope.error = "You must enter both an email and a password."
+            }
+        });
+    };
+
+    $scope.createUser = function() {
+        $scope.message = null;
+        $scope.error = null;
+
+        $scope.auth.$createUser({
+            email: $scope.addUserEmail,
+            password: $scope.addUserPassword
+        }).then(function(userData) {
+            $('#addUserModal').modal('hide')
+            $scope.alert = 'User created.'
+        }).catch(function(error) {
+            if(error.code === 'INVALID_PASSWORD' || error.code === 'INVALID_USER' || error.code === 'INVALID_EMAIL') {
+                $scope.error = error.message
+            } else {
+                console.log(error.code)
+                $scope.error = "You must enter both an email and a password."
+            }
+        });
+    };
+})
